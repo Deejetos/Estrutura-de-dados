5e4fc8f5e4dee7d8c1afc82e18ee246e40f65b97
Foxxex <98045648+Deejetos@users.noreply.github.com>
2025-03-18 12:22:57 -0300

    Add files via upload

---
+++ estrutura de dados/arrays.js
@@ -0,0 +1,54 @@
+/*
+let array = [1,2,3,4]
+array.push(5);
+console.log(array)
+*/
+
+/*
+let array = [10,20,30,40]
+let last=array.pop()
+console.log(array)
+*/
+
+/*
+let array = [5,10,15,20]
+let last = array.shift()
+console.log(array)
+*/
+
+/*
+let array = [1,2,3]
+array.unshift(0);
+console.log(array)
+*/
+
+/*
+let array = [10,20,30,40]
+array.splice(2,0,"novo")
+console.log(array)
+*/
+
+/*
+let array = [1,2,3]
+let dobro = array.map(item => item * 2)
+console.log(dobro)
+*/
+
+/*
+let arrayUm = [1,2,3]
+let arrayDois = [4,5,6]
+
+let combined = arrayUm.concat(arrayDois)
+console.log(combined)
+*/
+
+/*
+let array = [10,20,30]
+array.forEach(item => console.log(item));
+*/
+
+/*
+let array = [2,4,6]
+let triplo = array.map(item => item *3)
+console.log(triplo)
+*/
